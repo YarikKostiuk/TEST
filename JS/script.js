ff1bc583d98fadafc4d93bd0e27e734d0fd52214
Yaroslav <yarikkostuk@ukr.net>
2024-09-09 17:19:11 +0300

    Begin

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answe = [];
+
+answe[answe.length] = prompt('Як вас звати?', '');
+answe[answe.length] = prompt('Скільки вам років?', '');
+answe[answe.length] = prompt('Де ви живете?', '');
+
+console.log(answe);
