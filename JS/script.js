979c50418fafec4b5918329db07c346a9e35315d
Yaroslav <yarikkostuk@ukr.net>
2024-09-25 13:22:46 +0300

    add_new_functions

--- JS/script.js
+++ JS/script.js
@@ -2,96 +2,88 @@
 
 /* Задание на урок:
 
-1) Первую часть задания повторить по уроку
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-P.S. Функции вызывать не обязательно */
-
-// --------------------------------------------------------------------Функція, яка отримує кількість фільмів які ви уже подивилися
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
-    }
-}
-
-start();
-// --------------------------------------------------------------------Функція, яка отримує кількість фільмів які ви уже подивилися
 
 // --------------------------------------------------------------------Головний об*єкт програми, де зберігаються данні.
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-// --------------------------------------------------------------------Головний об*єкт програми, де зберігаються данні.
-
-// --------------------------------------------------------------------Функція, яка перевіряє приватність бази данних
-function showMyDB() {
-    if (personalMovieDB.privat !== false) {
-        console.log('Це приватна база данних!!!');
-    } else {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB();
-// --------------------------------------------------------------------Функція, яка перевіряє приватність бази данних
-
-// --------------------------------------------------------------------Функція, яка дізнається про улюблені жанри
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++ ) {
-        personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}?`);
-    }
-}
-
-writeYourGenres();
-// --------------------------------------------------------------------Функція, яка дізнається про улюблені жанри
-
-// --------------------------------------------------------------------Функція, яка дізнається про останні переглянуті фільми і їх оцінку
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один із останніх фільмів?', ''),
-              b = prompt('На скільки ви його оцінюєте?', '');
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Скільки фільмів ви уже подивилися?', '');
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Скільки фільмів ви уже подивилися?', '');
         }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один із останніх фільмів?', ''),
+                  b = prompt('На скільки ви його оцінюєте?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Переглянуто доволі мало фільмів");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Ви - класний глядач");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Ви - кіноман");
+        } else {
+            console.log("Виникла помилка :(");
+        }
+    },
+    showMyDB: function() {
+        if (personalMovieDB.privat !== false) {
+            console.log('Це приватна база данних!!!');
+        } else {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++ ) {
+            let genre = prompt(`Ваш улюблений жанр під номером ${i}?`);
+            
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            } 
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Улюблений жанр ${i + 1} - це ${item}`);
+        });
     }
-}
-
-rememberMyFilms();
-// --------------------------------------------------------------------Функція, яка дізнається про останні переглянуті фільми і їх оцінку
-
-// --------------------------------------------------------------------Функція, яка дає оцінку глядачу
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Переглянуто доволі мало фільмів");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Ви - класний глядач");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Ви - кіноман");
-    } else {
-        console.log("Виникла помилка :(");
-    }
-}
-
-detectPersonalLevel();
-// --------------------------------------------------------------------Функція, яка дає оцінку глядачу
+};
 
 console.log(personalMovieDB);
